04e11027b9482850c3b9c83fba2a720fe8c4f227
nghia <nghianm2803@gmail.com>
2023-06-16 22:50:29 +0700

    add get car list

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,83 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const { sendResponse, AppError } = require("../helpers/utils.js");
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const car = {
+    data: "car",
+    flag: false,
+  };
+
+  try {
+    //always remember to control your inputs
+    if (!car) throw new AppError(402, "Bad Request", "Create Car Error");
+    //mongoose query
+    const created = await Car.create(car);
+    sendResponse(res, 200, true, { data: created }, null, "Create Car Success");
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  //in real project you will getting condition from from req then construct the filter object for query
+  // empty filter mean get all
+  const filter = {};
+  try {
+    //mongoose query
+    // await collection.updateMany({}, { $set: { isDeleted: false } });
+    const listOfFound = await Car.find(filter).limit(5);
+    const totalCar = await Car.countDocuments(filter);
+
+    sendResponse(
+      res,
+      200,
+      true,
+      listOfFound,
+      null,
+      "Get Car List Successfully!",
+      totalCar
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  //in real project you will getting id from req. For updating and deleting, it is recommended for you to use unique identifier such as _id to avoid duplication
+  //you will also get updateCar from req
+  // empty target and info mean update nothing
+  const targetId = null;
+  const updateCar = "";
+
+  //options allow you to modify query. e.g new true return lastest update of data
+  const options = { new: true };
+  try {
+    //mongoose query
+    const updated = await Car.findByIdAndUpdate(targetId, updateCar, options);
+
+    sendResponse(res, 200, true, { data: updated }, null, "Update car success");
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  //in real project you will getting id from req. For updating and deleting, it is recommended for you to use unique identifier such as _id to avoid duplication
+
+  // empty target mean delete nothing
+  const targetId = null;
+  //options allow you to modify query. e.g new true return lastest update of data
+  const options = { new: true };
+  try {
+    //mongoose query
+    const updated = await Car.findByIdAndDelete(targetId, options);
+
+    sendResponse(res, 200, true, { data: updated }, null, "Delete car success");
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = carController;
---
+++ helpers/utils.js
@@ -0,0 +1,34 @@
+const utilsHelper = {};
+
+utilsHelper.sendResponse = (
+  res,
+  status,
+  success,
+  cars,
+  errors,
+  message,
+  totalCar
+) => {
+  const response = {};
+  if (success) response.success = success;
+  if (message) response.message = message;
+  if (cars) response.cars = cars;
+  if (totalCar) response.totalCar = totalCar;
+  if (errors) response.errors = errors;
+  return res.status(status).json(response);
+};
+
+class AppError extends Error {
+  constructor(statusCode, message, errorType) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorType = errorType;
+    // all errors using this class are operational errors.
+    this.isOperational = true;
+    // create a stack trace for debugging (Error obj, void obj to avoid stack polution)
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+utilsHelper.AppError = AppError;
+module.exports = utilsHelper;
